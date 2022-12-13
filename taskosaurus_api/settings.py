c264447e4395c7d0f3ae1c0fc03f33924e656036
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-13 14:32:43 +0000

    feat: start building profile model and methods to auto-create from user

--- taskosaurus_api/settings.py
+++ taskosaurus_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 
 if os.path.exists('env.py'):
     import env
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
