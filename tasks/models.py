29c35b33516ac6d8e4e09cbd928b231cf8ad0c1a
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-14 14:43:54 +0000

    feat: add first attempt at task model and urls to api

---
+++ tasks/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from profiles.models import Profile
+
+
+class Task (models.Model):
+    CURRENT = 'CU'
+    ARCHIVED = 'AC'
+    STATE_CHOICES = [
+        (CURRENT, 'current'),
+        (ARCHIVED, 'archived')
+    ]
+    MUST_DO = 'MUD'
+    MIGHT_DO = 'MID'
+    CAN_DO = 'CAD'
+    PRIORITY_CHOICES = [
+        (MUST_DO, 'must do'),
+        (MIGHT_DO, 'might do'),
+        (CAN_DO, 'can do')
+    ]
+    due_date = models.DateField(blank=False)
+    date_created = models.DateField(auto_now_add=True)
+    state = models.CharField(
+        max_length=2, choices=STATE_CHOICES, default=CURRENT
+    )
+    title = models.CharField(blank=False, max_length=100)
+    description = models.TextField(blank=True, max_length=500)
+    priority = models.CharField(
+        max_length=3, choices=PRIORITY_CHOICES, default=MUST_DO
+    )
+    account = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['state', '-date_created']
+
+    def __str__(self):
+        return f'{title}'
