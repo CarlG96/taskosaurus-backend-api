f9c5be8f587e3aa92bdfe2717e536632440e0cb5
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-21 11:31:31 +0000

    refactor: Refactored note views to generics

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status, permissions, filters
+from rest_framework import status, permissions, filters, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Note
@@ -7,57 +7,19 @@ from .serializers import NoteSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
-class NoteList(APIView):
+class NoteList(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
-    filter_backends = [filters.SearchFilter]
-    filter_fields = ['owner', 'title', 'task']
-    search_fields = ['owner', 'title', 'task']
+    queryset = Note.objects.all()
 
-    def get(self, request):
-        notes = Note.objects.all()
-        serializer = NoteSerializer(notes, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = NoteSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save()
 
 
-class NoteDetailView(APIView):
+class NoteDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
-    def get_object(self, pk):
-        try:
-            note = Note.objects.get(pk=pk)
-            task = note.task
-            self.check_object_permissions(self.request, note)
-            return note
-        except Note.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(note, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(note, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        note = self.get_object(pk)
-        note.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Note.objects.all()
