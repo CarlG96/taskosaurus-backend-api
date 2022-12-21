6465e8b2350d467ac07e9cb911ccd36b9d81ebcb
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-21 12:10:34 +0000

    feat: Add filters to task and notes

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, permissions, filters, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -13,7 +14,8 @@ class NoteList(generics.ListCreateAPIView):
         permissions.IsAuthenticatedOrReadOnly
     ]
     queryset = Note.objects.all()
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['task', 'task__owner__profile']
     search_fields = ['task__owner__username', 'title']
     
     def perform_create(self, serializer):
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, permissions, generics, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -11,7 +12,8 @@ class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Task.objects.all()
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['owner__profile', 'state', 'priority']
     search_fields = ['owner__username', 'title', 'state', 'priority']
 
     def perform_create(self, serializer):
