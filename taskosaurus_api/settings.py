5160d30d9d2e65425b381eea906260e30c2b6236
CarlG96 <carl.gardiner1@hotmail.com>
2023-02-19 11:51:20 +0000

    fix: Change refresh token settings

--- taskosaurus_api/settings.py
+++ taskosaurus_api/settings.py
@@ -52,7 +52,7 @@ if 'DEV' not in os.environ:
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=60*24),
     'ROTATE_REFRESH_TOKENS': True,
 }
 
